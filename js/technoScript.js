ca00574695f45a21735edb0ee532241099d991db
Sergey Pashnin <sspinit88@gmail.com>
2018-08-23 17:06:30 +0300

    validation

--- js/technoScript.js
+++ js/technoScript.js
@@ -17,6 +17,42 @@ $(document).ready(function () {
                 scrollTop: $('.header').offset().top - 50
             },1000);
         });
+
+    /*Валидация формы*/
+    let $form = $('.formColl');
+    let inp = $('.formColl > input');
+    let patterns = {
+        name: /^[a-zA-Zа-яА-Я0-9]+$/,
+        tell: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
+    };
+
+    $form.on('submit', function (e) {
+
+        let err = false;
+
+        for(let i = 0; i < inp.length; i++){
+            let val = inp.eq(i).val().trim();
+            let validation = inp.eq(i).attr('data-validation');
+            let pattern = (validation in patterns ) ? patterns[validation] : /.*/;
+
+            if(pattern.test(val)){
+                inp.eq(i).removeClass('err');
+            }
+            else {
+                inp.eq(i).addClass('err');
+                err = true;
+            }
+        }
+
+        if (err){
+            e.preventDefault();
+        }
+
+
+
+    }); // submit
+
+
     /*Слайдер*/
     $('.mainSlider').slick({
         dots: true,
